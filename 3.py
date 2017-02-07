0b6c0913d2c9a95732a0a6eb0926502dce2ddf40
Nathan <njyoung94@gmail.com>
2017-02-07 13:40:17 -0500

    Finished 3

---
+++ 3.py
@@ -0,0 +1,14 @@
+# Write a custom function called factorial() to calculate and display x! (x factorial) when passed an integer x,
+# and call it to show the 10! = 3628800. Note: do not use the existing math.factorial(x) maodule - you must
+# create your own implementation of the factorial function.
+
+
+def factorial(x):
+    b = 1
+    for a in range(1, x+1):
+        b = b*a
+    return b
+
+for a in range(0, 11):
+    print(factorial(a))
+
