78ea21b4d34564508f5ad17923a439e8791d855d
Nathan <njyoung94@gmail.com>
2017-02-07 13:20:31 -0500

    Finished 1

---
+++ 1.py
@@ -0,0 +1,8 @@
+# Using nested for loops, count from 1 to 5, repeated 3 times, with the output printed to the terminal.
+# Hint: remember that range(a,b) = (a, a+1, a+2, ..., b-1)
+
+for a in range(3):
+    for b in range(5):
+        print(b+1)
+
+
